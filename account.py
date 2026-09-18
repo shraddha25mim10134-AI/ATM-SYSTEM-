e4af3dba4e728130726a8203a829fe7b475c0896
shraddha25mim10134-AI <shraddha.25mim10134@vitbhopal.ac.in>
2026-09-18 18:59:23 +0530

    Create account.py

---
+++ account.py
@@ -0,0 +1,16 @@
+#account.py 
+# Shared account data
+
+account = {
+    
+    "pin" : "1234",
+    "balance" : 3000.0
+
+}
+
+
+def get_pin():
+    return "pin"
+
+def get_balance():
+    return float("balance")
